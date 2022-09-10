724298125e7125b8e0ed2026e63dc776c42e725d
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-09-10 22:35:34 +0530

    added footer section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
 const input = document.getElementById('link-input')
-const linkForm = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
 linkForm.addEventListener('submit', formSubmit)
+btn.addEventListener('click', navToggle)
+//Toggle Mobile Menu
 
-function formSubmit() {
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+function formSubmit(e) {
   e.preventDefault()
   if (input.value === '') {
-    errMsg.classList.add('border-red')
+    errMsg.innerHTML = 'Please enter something'
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid URL'
+    input.classList.add('border-red')
   }
 }
